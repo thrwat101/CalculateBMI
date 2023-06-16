3f2595d1810f63698e6ee83afe8792c3db91b205
thrwat101 <31253314+thrwat101@users.noreply.github.com>
2023-06-16 12:28:29 -0400

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -1,6 +1,6 @@
 /**
- * This program illustrates the use of the if-else-if structure 
- * and determines a person's BMI based on weight and height. The program
+ * This program illustrates the use of the Java methods and the if-else-if structure. 
+ * this program determines a person's BMI based on weight and height. The program
  * then gives the person their weight status.
  *
  * @author Thrwat Najjar
@@ -8,54 +8,68 @@
 import java.util.Scanner;
 public class BMI
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         double heightFeet = 0;
         double heightInches = 0;
         double weightPounds = 0;
-        String fullName = "";
         Scanner in = new Scanner(System.in);
 
-        //Gather input
-        System.out.println("Enter your name (first last):");
-        fullName = in.nextLine();
-        
-        System.out.println("Enter your weight in pounds (e.g. 175):");
-        weightPounds = in.nextDouble();
-        
+        //Height input
         System.out.println("Enter your height in feet and inces (e.g. 5 11):");
         heightFeet = in.nextDouble();
         heightInches = in.nextDouble();
+
+        //Convert height to meters
+        double heightMeters = getMeters(heightFeet, heightInches);
         
-        System.out.println();
+        //Weight input
+        System.out.println("Enter your weight in pounds (e.g. 175):");
+        weightPounds = in.nextDouble();
+
+        //Convert weight to Kg
+        double weightKg = getKg(weightPounds);
         
-        System.out.println("Body Mass Index Calculator");
-        System.out.println("==========================");
+        //Calculate BMI using our function
+        double BMI = calculateBMI(heightMeters, weightKg);
+
+        //Print output
+        System.out.println("BMI: " + BMI);
+        System.out.println("Category: " + getWeightStatus(BMI));
+    }
+    public static double calculateBMI(double height, double weight){
         
-        //Calculate BMI, Height in Meters, and Weight in KG
-        double heightMeters = (heightFeet * .3) + (heightInches * .025);
-        heightMeters = (double)((int)((heightMeters + .05) *10.0))/10.0;
-        double weightKg = (weightPounds * .4535);
-        weightKg = (double)((int)((weightKg + .05) *10.0))/10.0;
-        double BMI = weightKg/(heightMeters * heightMeters);
-        BMI = (double)((int)((BMI + .05) *10.0))/10.0;
+        double bmi = weight/(height*height);
+        bmi = (double)((int)((bmi + .05) *10.0))/10.0;
+    
+        return bmi;
+    }
+    public static String getWeightStatus(double BMI){
         String weightStatus = "";
 
-        //Determine weight status based on BMI
         if(BMI >= 29.9)
             weightStatus = "Obese";
-        else if(BMI >= 25.0 && BMI <= 29.9)
+        else if(BMI >= 25.0 && BMI < 29.9)
             weightStatus = "Overweight";
         else if(BMI >= 18.5 && BMI <= 24.9)
             weightStatus = "Normal or Healthy Weight";
-        else if(BMI <= 18.5)
+        else if(BMI <18.5)
             weightStatus = "Underweight";
-
-        //Print output
-        System.out.println("Name: " + fullName);
-        System.out.println("Height (m): " + heightMeters);
-        System.out.println("Weight (kg): " + weightKg);
-        System.out.println("BMI: " + BMI);
-        System.out.println("Category: " + weightStatus);
+        
+        return weightStatus;
     }
+
+    public static double getMeters(double heightFeet, double heightInches){
+        double heightMeters = (heightFeet * .3) + (heightInches * .025);
+        heightMeters = (double)((int)((heightMeters + .05) *10.0))/10.0;
+
+        return heightMeters;
+    }
+
+    public static double getKg(double weightPounds){
+        double weightKg = (weightPounds * .4535);
+        weightKg = (double)((int)((weightKg + .05) *10.0))/10.0;
+
+        return weightKg;
+    }
+
 }
